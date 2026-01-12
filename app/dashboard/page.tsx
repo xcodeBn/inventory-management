2502bdae5c34a239b75ffab5cef36462cd667f3f
XcodeBn <86073708+xcodebn@users.noreply.github.com>
2026-01-12 13:54:05 +0200

    sidebar.tsx done
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default function DashboardPage() {
 
     return(
         <div className="min-h-screen bg-gray-50">
-        <SideBar currentPath="/dashboard" />
-    </div>
+            <SideBar currentPath="/dashboard" />
+        </div>
     );
 }
--- components /sidebar.tsx
+++ components /sidebar.tsx
@@ -1,17 +1,65 @@
-import {AppleIcon} from "lucide-react";
+import {AppleIcon, BarChart, BarChart3, Package, Plus, Settings} from "lucide-react";
+import Link from "next/link";
+import {UserButton} from "@neondatabase/auth/react";
 
 
 export default function SideBar({currentPath = "/dashboard"}:{
     currentPath: string;
 }) {
-
+    const navigation = [
+        {
+            name: "Dashboard",href: "/dashboard", icon: BarChart3,
+        },
+        {
+            name: "Inventory",href: "/inventory", icon: Package,
+        },
+        {
+            name: "Add Product",href: "/add-product", icon: Plus,
+        },
+        {
+            name: "Settings",href: "/settings", icon: Settings,
+        },
+    ]
 
     return (
         <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen p-6">
             <div className={"mb-8"}>
                 <div className={"flex items-center space-x-2 mb-4"}>
-                    <AppleIcon/>
+                    <BarChart3 className={"w-7 h-7"}/>
+                    <span className={"text-lg font-semibold"}>
+                    Inventory App
+                    </span>
                 </div>
+
+                <nav className={"space-y-1"}>
+                    <div className={"text-sm font-semibold text-gray-400 uppercase"}>
+                        Inventory
+                    </div>
+                    {
+
+                        navigation.map((item,key)=>{
+                            const IconComponent = item.icon;
+                            const isActive : boolean  = currentPath === item.href;
+                            return(
+                                <Link href={item.href} key={key}
+                                      className={`flex items-center space-x-3 py-2 px-3 rounded-lg 
+                                      ${isActive?"bg-purple-100 text-gray-800": "text-gray-300 hover:bg-gray-800"} `} >
+                                    <IconComponent className={"w-5 h-5"}/>
+                                    <span className={"text-sm"}>
+                                    {item.name}
+                                    </span>
+                                </Link>
+                            )
+                        })
+                    }
+                </nav>
+                <div className={"absolute bottom-0 left-0 right-0 p-6 border-t border-gray-700 overflow-hidden"}>
+                    <div className={"flex items-center  mb-4 justify-between"}>
+                        <UserButton   className="max-w-full overflow-hidden"/>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
